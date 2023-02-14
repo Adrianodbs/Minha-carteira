62e863489ec1598e8a8add3d34b82b816d232c3c
Adriano <adriano_alves_2008@hotmail.com>
2023-02-14 20:48:09 -0300

    Styled Components - Instalação e estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+import { Global } from './styles/GlobalStyles'
+
+function App() {
+  return (
+    <>
+      <Global />
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
