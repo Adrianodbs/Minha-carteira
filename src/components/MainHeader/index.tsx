a39f6d882a798e842fb8acebf6511802ed03be4b
Adriano <adriano_alves_2008@hotmail.com>
2023-02-15 15:16:52 -0300

    Criando o header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,23 @@
-import { Container } from './styles'
+import { useMemo } from 'react'
+
+import emojis from '../../utils/emojis'
+
+import * as C from './styles'
 
 function MainHeader() {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length)
+    return emojis[indice]
+  }, [])
   return (
-    <Container>
-      <h1>MainHeader</h1>
-    </Container>
+    <C.Container>
+      <h1>Toggle</h1>
+
+      <C.Profile>
+        <C.Welcome>Olá, {emoji}</C.Welcome>
+        <C.UserName>Adriano Alves</C.UserName>
+      </C.Profile>
+    </C.Container>
   )
 }
 
