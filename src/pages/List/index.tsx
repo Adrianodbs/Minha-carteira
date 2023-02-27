12a0f3391d1954aeaa28189b586af193d9d0ddeb
Adriano <adriano_alves_2008@hotmail.com>
2023-02-26 21:08:33 -0300

    Carregando os meses e anos dinamicamente

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -7,9 +7,12 @@ import gains from '../../repositories/gains'
 import expenses from '../../repositories/expenses'
 import formatCurrency from '../../utils/formatCurrency'
 import formatDate from '../../utils/formatDate'
+import listOfMonths from '../../utils/months'
 
 import { useMemo, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+
+import { v4 as uuidv4 } from 'uuid'
 
 interface IData {
   id: string
@@ -44,26 +47,34 @@ function List() {
     return type === 'entry-balance' ? gains : expenses
   }, [type])
 
-  const months = [
-    { value: 1, label: 'Janeiro' },
-    { value: 2, label: 'Fevereiro' },
-    { value: 3, label: 'Março' },
-    { value: 4, label: 'Abril' },
-    { value: 5, label: 'Maio' },
-    { value: 6, label: 'Junho' },
-    { value: 7, label: 'Julho' },
-    { value: 8, label: 'Agosto' },
-    { value: 9, label: 'Setembro' },
-    { value: 10, label: 'Outubro' },
-    { value: 11, label: 'Novembro' },
-    { value: 12, label: 'Dezembro' }
-  ]
+  const years = useMemo(() => {
+    let uniqueYears: number[] = []
 
-  const years = [
-    { value: 2023, label: 2023 },
-    { value: 2024, label: 2024 },
-    { value: 2025, label: 2025 }
-  ]
+    listData.forEach(item => {
+      const date = new Date(item.date)
+      const year = date.getFullYear()
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year)
+      }
+    })
+
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year
+      }
+    })
+  }, [listData])
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month
+      }
+    })
+  }, [])
 
   useEffect(() => {
     const filteredData = listData.filter(item => {
@@ -76,7 +87,7 @@ function List() {
 
     const formattedData = filteredData.map(item => {
       return {
-        id: String(new Date().getTime()) + item.amount,
+        id: uuidv4(),
         description: item.description,
         amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
